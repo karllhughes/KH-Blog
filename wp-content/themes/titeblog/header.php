6c6b017975497e3fa31a51ed02fda787c5aa05b6
Karl <khughes.me@gmail.com>
2015-12-04 15:40:21 -0600

    CUstom theme

--- wp-content/themes/titeblog/header.php
+++ wp-content/themes/titeblog/header.php
@@ -34,7 +34,6 @@
 					<!-- logo -->
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
 							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
 						</a>
 					</div>
@@ -42,7 +41,7 @@
 
 					<!-- nav -->
 					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
+						<?php titeblog_nav(); ?>
 					</nav>
 					<!-- /nav -->
 
