0e1707cf9645a7a700f500834f9b0a257004b910
Karl Hughes <khughes.me@gmail.com>
2015-12-05 14:59:46 -0500

    Adding social links to header

--- wp-content/themes/titeblog/header.php
+++ wp-content/themes/titeblog/header.php
@@ -29,8 +29,8 @@
 		<div class="wrapper">
 
 			<!-- header -->
-			<header class="header clear row" role="banner">
-				<div class="small-12 columns">
+			<header class="header clear row-fluid" role="banner">
+				<div class="medium-8 medium-offset-2 columns">
 					<!-- logo -->
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
@@ -42,6 +42,15 @@
 					</div>
 					<!-- /logo -->
 				</div>
-
+				<div class="medium-2 columns">
+					<!-- social -->
+					<div class="social">
+						<a href="https://www.linkedin.com/in/karllhughes" target="_blank"><i class="foundicon-linkedin"></i></a>
+						<a href="https://github.com/karllhughes" target="_blank"><i class="foundicon-github"></i></a>
+						<a href="https://twitter.com/karllhughes" target="_blank"><i class="foundicon-twitter"></i></a>
+						<a href="<?php echo site_url('rss'); ?>" target="_blank"><i class="foundicon-rss"></i></a>
+					</div>
+					<!-- /social -->
+				</div>
 			</header>
 			<!-- /header -->
