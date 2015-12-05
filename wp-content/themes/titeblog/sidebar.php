d7737db2503fc32c17b607e78ebe727849d24335
Karl Hughes <khughes.me@gmail.com>
2015-12-05 17:10:38 -0500

    Styling more good stuff

--- wp-content/themes/titeblog/sidebar.php
+++ wp-content/themes/titeblog/sidebar.php
@@ -9,12 +9,5 @@
 		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-2')) ?>
 	</div>
 
-	<?php if( current_user_can('administrator')) { ?>
-	<p>
-		Queries on this page: <?php echo get_num_queries(); ?><br/>
-		<?php edit_post_link(); ?>
-	</p>
-	<?php } ?>
-
 </aside>
 <!-- /sidebar -->
